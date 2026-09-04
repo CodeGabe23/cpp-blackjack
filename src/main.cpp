5d4485f1fceb89bbdc4da6cd9ebabbb2256eabfa
CodeGabe23 <gabe@codegabe.com>
2026-09-04 15:00:59 +0100

    Added background music

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 	//inits
-    Game game;
+  Game game;
 	Player player;
 
 	constexpr int SCREEN_WIDTH {850};
@@ -15,27 +15,37 @@ int main()
 	bool hasPayedOutPot = false;
 
 	InitWindow(850,531, "C++ Blackjack");
+  InitAudioDevice();
 
 	// textures/images
 	// NOTE: textures must be loaded after window init 
 	// cuz OpenGL context is requried
-	Texture2D bg		= LoadTexture("images/bg.jpg");
-	Texture2D deal		= LoadTexture("images/deal.png");
-	Texture2D hit		= LoadTexture("images/hit.png");
+	Texture2D bg		    = LoadTexture("images/bg.jpg");
+	Texture2D deal		  = LoadTexture("images/deal.png");
+	Texture2D hit		    = LoadTexture("images/hit.png");
 	Texture2D play_again= LoadTexture("images/play_again.png");
-	Texture2D stand		=LoadTexture("images/stand.png");
-	Texture2D bet10		= LoadTexture("images/bet10.png");
+	Texture2D stand		  = LoadTexture("images/stand.png");
+	Texture2D bet10		  = LoadTexture("images/bet10.png");
+
+  // Music
+  Music gameMusic = LoadMusicStream("music/jazz.mp3");
+  float volume = 0.5f;
+  SetMusicVolume(gameMusic, volume);
+  
+  PlayMusicStream(gameMusic);
 
 	// for rectangle button bounds
-	Rectangle dealBounds		= {SCREEN_WIDTH / 3 + 50, SCREEN_HEIGHT  / 2,			161, 47};
-	Rectangle hitBounds			= {SCREEN_WIDTH / 2 - 300, SCREEN_HEIGHT / 3,			165, 55};
-	Rectangle standBounds		= {SCREEN_WIDTH / 2,		SCREEN_HEIGHT/ 3,			167, 53};
+	Rectangle dealBounds		  = {SCREEN_WIDTH / 3 + 50, SCREEN_HEIGHT  / 2,			161, 47};
+	Rectangle hitBounds			  = {SCREEN_WIDTH / 2 - 300, SCREEN_HEIGHT / 3,			165, 55};
+	Rectangle standBounds	  	= {SCREEN_WIDTH / 2,		SCREEN_HEIGHT/ 3,			167, 53};
 	Rectangle playAgainBounds	= {SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 - 50,207, 50};
-	Rectangle bet10Bounds		= {SCREEN_WIDTH / 3 + 30, SCREEN_HEIGHT  / 2, 161, 47};
+	Rectangle bet10Bounds		  = {SCREEN_WIDTH / 3 + 30, SCREEN_HEIGHT  / 2, 161, 47};
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
+    UpdateMusicStream(gameMusic);
+
 		BeginDrawing();
 		// Place BG image
 		DrawTexture(bg, 0, 0, WHITE);
@@ -202,10 +212,9 @@ int main()
 						std::cout << "\n\nGame State: " << game.CheckGameState();
 					}
 				}	
-
-
 				break;
-			default:
+	
+      default:
 				std::cout << "ERROR: game state checking failed";
 		}
 		
@@ -220,6 +229,9 @@ int main()
 	UnloadTexture(play_again);
 	UnloadTexture(stand);
 	
+  UnloadMusicStream(gameMusic);
+  CloseAudioDevice();
+
 	CloseWindow();
 
 	return 0;
